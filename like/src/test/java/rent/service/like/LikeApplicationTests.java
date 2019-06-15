6be78164c7557bc92406c52388ef249b5b534dd3
mohammad <mohammad-alsharif@zad-solution.com>
2019-06-16 01:59:51 +0200

    Initial commit

---
+++ like/src/test/java/rent/service/like/LikeApplicationTests.java
@@ -0,0 +1,16 @@
+package rent.service.like;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LikeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
